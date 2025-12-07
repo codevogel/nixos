d33244d11af3b953e6d5254fde2cdacaf1b7f278
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-07 14:37:46 +0100

    move locale to module

--- hosts/home-nest/configuration.nix
+++ hosts/home-nest/configuration.nix
@@ -9,30 +9,13 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       inputs.home-manager.nixosModules.default
-      ../../modules/system/fonts.nix
-      ../../modules/system/boot.nix
-      ../../modules/system/networking.nix
+      ../../modules/system/fonts.nix # Fonts
+      ../../modules/system/boot.nix # Boot options (loader, kernel)
+      ../../modules/system/networking.nix # Networking
+      ../../modules/system/locale.nix # Locale (time, internationalization)
     ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    # Set your time zone.
-  time.timeZone = "Europe/Amsterdam";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_GB.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "nl_NL.UTF-8";
-    LC_IDENTIFICATION = "nl_NL.UTF-8";
-    LC_MEASUREMENT = "nl_NL.UTF-8";
-    LC_MONETARY = "nl_NL.UTF-8";
-    LC_NAME = "nl_NL.UTF-8";
-    LC_NUMERIC = "nl_NL.UTF-8";
-    LC_PAPER = "nl_NL.UTF-8";
-    LC_TELEPHONE = "nl_NL.UTF-8";
-    LC_TIME = "nl_NL.UTF-8";
-  };
 
   # Configure keymap in X11
   services.xserver.xkb = {
---
+++ modules/system/locale.nix
@@ -0,0 +1,22 @@
+{ config, ...}:
+
+{
+  # Set your time zone.
+  time.timeZone = "Europe/Amsterdam";
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "nl_NL.UTF-8";
+      LC_IDENTIFICATION = "nl_NL.UTF-8";
+      LC_MEASUREMENT = "nl_NL.UTF-8";
+      LC_MONETARY = "nl_NL.UTF-8";
+      LC_NAME = "nl_NL.UTF-8";
+      LC_NUMERIC = "nl_NL.UTF-8";
+      LC_PAPER = "nl_NL.UTF-8";
+      LC_TELEPHONE = "nl_NL.UTF-8";
+      LC_TIME = "nl_NL.UTF-8";
+    };
+  };
+}
