d6b95009b0acf8b79fb9ec6de930e6a84b5cce53
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-13 21:01:31 +0100

    add blink.cmp

--- modules/mnw/mnw.nix
+++ modules/mnw/mnw.nix
@@ -7,21 +7,36 @@
 
     luaFiles = [ ./nvim/init.lua ];
 
+    # Along with listing the package here, each plugin should also be
+    # defined through lua as per the Lazy.nvim spec
+    #
+    # Note: Plugins with dependencies should have their dependencies defined individually
+    #       Both as a pkg in the list below, AND in a .lua file as per the Lazy.nvim spec
+    #       (A plugin that is only listed as a dependency using the { dependencies } attribute
+    #       will make nmw throw errors if is it not sourced individually.)
     plugins = {
       start = [
         pkgs.vimPlugins.lazy-nvim
       ];
 
+      # Anything that is loaded by Lazy.nvim needs to be in opt 
+      # use 'lazy = false' in the lazy spec to not lazy-load any plugins
+      # that are configured through Lazy.nvim
       opt = [
         pkgs.vimPlugins.plenary-nvim
+
         pkgs.vimPlugins.oil-nvim
         pkgs.vimPlugins.snacks-nvim
         pkgs.vimPlugins.lualine-nvim
         pkgs.vimPlugins.nvim-web-devicons
+        pkgs.vimPlugins.friendly-snippets
       ];
 
+      # We can use optAttrs to rename plugins to their expected name
+      # as defined by the lua files
       optAttrs = {
         harpoon = pkgs.vimPlugins.harpoon2;
+        "blink.cmp" = pkgs.vimPlugins.blink-cmp;
       };
 
       dev.codevogel = {
