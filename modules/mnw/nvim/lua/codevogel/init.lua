fee25b9b2e816b765b554bd30c9c8cfea74c7c29
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-14 21:44:31 +0100

    add highlighting for svelte

--- modules/mnw/nvim/lua/codevogel/init.lua
+++ modules/mnw/nvim/lua/codevogel/init.lua
@@ -3,3 +3,10 @@ require("codevogel.config.diagnostic")
 require("codevogel.config.keymaps")
 
 vim.cmd("colorscheme kanagawa")
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "svelte" },
+  callback = function()
+    vim.treesitter.start()
+  end,
+})
