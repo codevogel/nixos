ecd0327737d330f1cc279f0306ce0b49e223e0a2
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-14 21:42:29 +0100

    add roslyn.lua

--- modules/mnw/nvim/lua/codevogel/plugins/mason.lua
+++ modules/mnw/nvim/lua/codevogel/plugins/mason.lua
@@ -1,6 +1,14 @@
 if mnw == nil then
   return {
     "mason-org/mason.nvim",
-    opts = {}
+    opts = {
+      ensure_installed = {
+        "stylua",
+      },
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+    },
   }
 end
