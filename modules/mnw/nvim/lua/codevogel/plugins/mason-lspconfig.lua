5ef9a74f468998fde0db32f8e6d6e3fc2ce5aabe
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-19 22:33:39 +0100

    disable mason in nix

--- modules/mnw/nvim/lua/codevogel/plugins/mason-lspconfig.lua
+++ modules/mnw/nvim/lua/codevogel/plugins/mason-lspconfig.lua
@@ -1,10 +1,14 @@
-if mnw == nil then
-  return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-      { "mason-org/mason.nvim", opts = {} },
-      "neovim/nvim-lspconfig",
-    },
-  }
+local cfg = {
+  "mason-org/mason-lspconfig.nvim",
+  opts = {},
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "neovim/nvim-lspconfig",
+  },
+}
+
+if mnw ~= nil then
+  cfg.enabled = false
 end
+
+return cfg
--- modules/mnw/nvim/lua/codevogel/plugins/mason.lua
+++ modules/mnw/nvim/lua/codevogel/plugins/mason.lua
@@ -1,14 +1,18 @@
-if mnw == nil then
-  return {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-      },
-      registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
-      },
+local plugin = {
+  "mason-org/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
     },
-  }
+    registries = {
+      "github:mason-org/mason-registry",
+      "github:Crashdummyy/mason-registry",
+    },
+  },
+}
+
+if mnw ~= nil then
+  plugin.enabled = false
 end
+
+return plugin
