cbd3b05eca460a150ca6cf63ba2401c66d3aae32
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-19 22:33:50 +0100

    remove treesitter setup

--- modules/mnw/nvim/lua/codevogel/plugins/nvim-treesitter.lua
+++ modules/mnw/nvim/lua/codevogel/plugins/nvim-treesitter.lua
@@ -2,10 +2,4 @@ return {
   "nvim-treesitter/nvim-treesitter",
   lazy = false,
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter").setup({
-      install_dir = vim.fn.stdpath("data") .. "/site",
-      require("nvim-treesitter").install({ "javascript", "svelte", "html", "typescript", "bash" })
-    })
-  end,
 }
