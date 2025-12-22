79d88ed09b8382b24f1af4bf7e6b2239cb5eb0a7
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-22 18:21:06 +0100

    fix nvidia suspend issue

--- modules/common/system/nvidia.nix
+++ modules/common/system/nvidia.nix
@@ -6,6 +6,7 @@
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.nvidiaSettings = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
+  hardware.nvidia.powerManagement.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
 
   # These are for Hyprland but might be useful for other programs too(?)
@@ -13,4 +14,6 @@
     __GLX_VENDOR_LIBRARY_NAME = lib.mkDefault "nvidia";
     LIBVA_DRIVER_NAME = lib.mkDefault "nvidia";
   };
+
+  boot.kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
 }
