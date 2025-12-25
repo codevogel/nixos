e223aa964f62667b3dea8a8fca3da4f7bf45ba5f
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-25 13:35:19 +0100

    add battery to waybar

--- modules/work-nest/home/wm/hypr/waybar/waybar.nix
+++ modules/work-nest/home/wm/hypr/waybar/waybar.nix
@@ -7,6 +7,7 @@
 
         modules-right = [
           "bluetooth"
+          "battery"
         ];
 
         "bluetooth" = {
@@ -17,6 +18,50 @@
           tooltip-format = "Devices connected: {num_connections}";
           on-click = "sh -c 'rfkill unblock bluetooth && kitty --title bluetui -e bluetui'";
           on-click-right = "sh -c 'bluetoothctl power off && rfkill block bluetooth'";
+        };
+
+        "battery" = {
+          format = "{capacity}% {icon}";
+          format-discharging = "{icon}";
+          format-charging = "{icon}";
+          format-plugged = "";
+
+          format-icons = {
+            charging = [
+              "󰢜"
+              "󰂆"
+              "󰂇"
+              "󰂈"
+              "󰢝"
+              "󰂉"
+              "󰢞"
+              "󰂊"
+              "󰂋"
+              "󰂅"
+            ];
+            default = [
+              "󰁺"
+              "󰁻"
+              "󰁼"
+              "󰁽"
+              "󰁾"
+              "󰁿"
+              "󰂀"
+              "󰂁"
+              "󰂂"
+              "󰁹"
+            ];
+          };
+
+          format-full = "󰂅";
+          tooltip-format-discharging = "{power:>1.0f}W↓ {capacity}%";
+          tooltip-format-charging = "{power:>1.0f}W↑ {capacity}%";
+          interval = 5;
+
+          states = {
+            warning = 20;
+            critical = 10;
+          };
         };
 
       };
