882fc952d54afc1dd1f826a1a7b1c47ca6503268
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-31 16:02:48 +0100

    add pls

---
+++ modules/shared/home/dev/pls/pls.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+let
+  pls = pkgs.stdenv.mkDerivation {
+    pname = "pls";
+    version = "0.1.0";
+
+    src = pkgs.fetchurl {
+      url = "https://github.com/codevogel/pls/releases/download/v0.1.0/pls";
+      sha256 = "1zpl5x8xlb8zp7zmhkpna5naz1znwb3iwfibmdqkiiznnkndzqf0";
+    };
+
+    unpackPhase = ''true'';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp $src $out/bin/pls
+      chmod +x $out/bin/pls
+    '';
+  };
+in
+{
+  home.packages = with pkgs; [
+    pls
+
+    # Dependencies:
+    yq-go
+    fzf
+  ];
+}
