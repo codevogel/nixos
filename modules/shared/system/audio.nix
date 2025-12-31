457f0f19fc5869f0f3e2b06bfc553f6d676e3bc6
Kamiel de Visser <kamieldevisser@gmail.com>
2025-12-31 15:20:37 +0100

    set samplerate to 96khz by default

--- modules/shared/system/audio.nix
+++ modules/shared/system/audio.nix
@@ -9,6 +9,18 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
+      jack.enable = true;
+      extraConfig.pipewire."10-samplerate" = {
+        context.properties = {
+          default.clock.rate = 96000;
+          default.clock.allowed-rates = [
+            44100
+            48000
+            88200
+            96000
+          ];
+        };
+      };
     };
   };
 }
