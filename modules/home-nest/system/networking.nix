4115a9bb0dd20da37b179521437629c20c1a81b9
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-17 15:54:31 +0100

    add ethernet speed

--- modules/home-nest/system/networking.nix
+++ modules/home-nest/system/networking.nix
@@ -1,7 +1,32 @@
 { lib, ... }:
 
 {
-  networking = {
-    hostName = lib.mkForce "home-nest"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  networking.hostName = lib.mkForce "home-nest"; # Define your hostname.
+
+  networking.networkmanager.ensureProfiles.profiles = {
+    wired-enp34s0 = {
+      connection = {
+        id = "Wired connection 1";
+        type = "802-3-ethernet";
+        interface-name = "enp34s0";
+        autoconnect = true;
+      };
+
+      ethernet = {
+        auto-negotiate = true;
+        speed = 1000;
+        duplex = "full";
+      };
+
+      ipv4 = {
+        method = "auto";
+      };
+
+      ipv6 = {
+        method = "auto";
+      };
+    };
   };
 }
