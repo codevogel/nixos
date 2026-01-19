2e7b4fed94beb940d0c0ad737fbc0ad8f5bd0ec8
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-19 11:05:43 +0100

    swap ctrl with caps on work-nest

--- modules/work-nest/home/wm/hypr/hyprland.nix
+++ modules/work-nest/home/wm/hypr/hyprland.nix
@@ -4,6 +4,9 @@
   wayland.windowManager.hyprland = {
     settings = {
       monitor = [ "eDP-1, 1920x1200, 0x0, 1" ];
+      input = {
+        kb_options = "ctrl:swapcaps";
+      };
     };
   };
 
