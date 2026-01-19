ea97f47dbf8ab77d82dd6eb453e2f6a295f550f9
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-19 13:19:31 +0100

    disable caps

--- modules/work-nest/home/wm/hypr/hyprland.nix
+++ modules/work-nest/home/wm/hypr/hyprland.nix
@@ -5,7 +5,7 @@
     settings = {
       monitor = [ "eDP-1, 1920x1200, 0x0, 1" ];
       input = {
-        kb_options = "ctrl:swapcaps";
+        kb_options = "ctrl:nocaps";
       };
     };
   };
