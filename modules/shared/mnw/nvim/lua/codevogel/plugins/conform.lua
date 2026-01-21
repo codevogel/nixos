a15f2ce51ad1e5962fa0fc51e5919213e22e1dca
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-21 16:13:31 +0100

    update conform opts

--- modules/shared/mnw/nvim/lua/codevogel/plugins/conform.lua
+++ modules/shared/mnw/nvim/lua/codevogel/plugins/conform.lua
@@ -1,20 +1,21 @@
 return {
   "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      nix = { "nixfmt" },
-      sh = { "beautysh" },
-      markdown = { "prettierd", "markdownlint-cli2" },
-    },
-  },
   config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        nix = { "nixfmt" },
+        sh = { "beautysh" },
+        markdown = { "prettierd", "markdownlint-cli2" },
+      },
+    })
     vim.api.nvim_create_autocmd("BufWritePre", {
       pattern = "*",
       callback = function(args)
         require("conform").format({
+          async = true,
           bufnr = args.buf,
-          timeout_ms = 500,
+          timeout_ms = 1000,
           lsp_format = "fallback",
         })
         require("hatch").hatch()
