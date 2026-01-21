97f755b738426f0b91cdb349e19cdda1906ef8d4
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-21 14:47:51 +0100

    add hatch opts

--- modules/shared/mnw/nvim/lua/codevogel/plugins/hatch.lua
+++ modules/shared/mnw/nvim/lua/codevogel/plugins/hatch.lua
@@ -1,4 +1,8 @@
 return {
-  dir = "~/work/neovim/hatch.nvim",
-  opts = {},
+  "codevogel/hatch.nvim",
+  opts = {
+    template_repository = "git@github.com:codevogel/hatch.nvim-templates.git",
+    template_directory = vim.fn.expand("~/.config/hatch.nvim/templates"),
+    create_autocmd = false,
+  },
 }
