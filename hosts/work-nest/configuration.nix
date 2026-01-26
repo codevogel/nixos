5a0385b49c3e46095abf6a07ae2730accdef7471
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-26 11:35:13 +0100

    share between configurations

--- hosts/work-nest/configuration.nix
+++ hosts/work-nest/configuration.nix
@@ -1,35 +1,14 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { ... }:
 
 {
   imports = [
-    # Include the results of the hardware scan.
+    ../shared.nix
     ./hardware-configuration.nix
     ../../modules/shared/system/system.nix
-    ../../modules/work-nest/system/nvidia.nix # Nvidia
+    ../../modules/work-nest/system/nvidia.nix
     ../../modules/work-nest/system/networking.nix
     ../../modules/work-nest/system/home-manager.nix
     ../../modules/work-nest/system/bluetooth.nix
     ../../modules/work-nest/system/theme.nix
   ];
-
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
-
 }
