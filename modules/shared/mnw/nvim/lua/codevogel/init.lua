855df43c8cac5256daf8e6a5b8402106e88ced26
Kamiel de Visser <kamieldevisser@gmail.com>
2026-01-29 22:31:11 +0100

    fix treesitter

--- modules/shared/mnw/nvim/lua/codevogel/init.lua
+++ modules/shared/mnw/nvim/lua/codevogel/init.lua
@@ -5,10 +5,10 @@ require("codevogel.config.keymaps")
 vim.cmd("colorscheme kanagawa")
 
 require("nvim-treesitter").setup({})
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = vim.treesitter.language._complete(),
-  group = vim.api.nvim_create_augroup("LoadTreesitter", {}),
-  callback = function()
-    vim.treesitter.start()
+  group = vim.api.nvim_create_augroup("LoadTreesitter", { clear = true }),
+  callback = function(args)
+  pcall(vim.treesitter.start, args.buf)
   end,
 })
