6094f1690fbb1565c86bf08fe8ee5496c879fffc
Kamiel de Visser <kamieldevisser@gmail.com>
2026-02-06 15:20:31 +0100

    set diagnostic colors

--- modules/shared/mnw/nvim/lua/codevogel/plugins/kanagawa.lua
+++ modules/shared/mnw/nvim/lua/codevogel/plugins/kanagawa.lua
@@ -1,3 +1,14 @@
 return {
   "rebelot/kanagawa.nvim",
+  opts = {
+    overrides = function(colors)
+      return {
+        DiagnosticWarn = {
+          fg = colors.palette.roninYellow,
+          bg = colors.palette.sumiInk0,
+          bold = false,
+        },
+      }
+    end,
+  },
 }
