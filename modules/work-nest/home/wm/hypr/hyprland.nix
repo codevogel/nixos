f28f599baf2b864b4edc046ca6f3fc46c5525dba
Kamiel de Visser <kamieldevisser@gmail.com>
2026-02-09 14:50:21 +0100

    add DP-2 to work-nest

--- modules/work-nest/home/wm/hypr/hyprland.nix
+++ modules/work-nest/home/wm/hypr/hyprland.nix
@@ -3,7 +3,10 @@
 {
   wayland.windowManager.hyprland = {
     settings = {
-      monitor = [ "eDP-1, 1920x1200, 0x0, 1" ];
+      monitor = [ 
+      "eDP-1, 1920x1200, 0x0, 1"
+      "DP-2, 1920x1080, 1920x0, 1"
+      ];
       input = {
         kb_options = "ctrl:nocaps";
       };
