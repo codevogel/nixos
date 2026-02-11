dc931dbc87c4d56bffd03800b00892d438a630ca
Kamiel de Visser <kamieldevisser@gmail.com>
2026-02-11 15:01:33 +0100

    change copilot accept bind

--- modules/shared/mnw/nvim/lua/codevogel/plugins/copilot.lua
+++ modules/shared/mnw/nvim/lua/codevogel/plugins/copilot.lua
@@ -1,3 +1,13 @@
 return {
-  "github/copilot.vim"
+  "github/copilot.vim",
+  config = function()
+    -- Disable default Tab mapping
+    vim.g.copilot_no_tab_map = true
+
+    -- Accept suggestion with Shift+Tab
+    vim.keymap.set("i", "<C-Tab>", 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+  end
 }
