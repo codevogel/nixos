3e3a3b7317faf2bad41bb61bc10b8c252a913536
Kamiel de Visser <kamieldevisser@gmail.com>
2026-02-19 12:20:02 +0100

    resolve blink & copilot conflict

--- modules/shared/mnw/nvim/lua/codevogel/plugins/copilot.lua
+++ modules/shared/mnw/nvim/lua/codevogel/plugins/copilot.lua
@@ -1,12 +1,10 @@
 return {
   "github/copilot.vim",
   config = function()
-    -- Disable default Tab mapping
-    vim.g.copilot_no_tab_map = true
-
     vim.keymap.set("i", "<C-p>", 'copilot#Accept("\\<CR>")', {
       expr = true,
-      replace_keycodes = true,
+      replace_keycodes = false,
     })
+    vim.g.copilot_no_tab_map = true
   end,
 }
