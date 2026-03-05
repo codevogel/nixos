048c681a625d877066c102e2dbb6198558939e44
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-05 18:26:07 +0100

    add notes to home-manager defaults

--- modules/common/home-manager/desktop-environment/default.nix
+++ modules/common/home-manager/desktop-environment/default.nix
@@ -13,7 +13,10 @@
     host-options.home-manager.desktop-environment.enable =
       lib.mkEnableOption "Enable home-manager.desktop-environment"
       // {
-        default = config.host-options.home-manager.enable;
+        # NOTE: This defaults to the value of config.system.desktop-environment.enable,
+        # so that if the user disables the desktop environment at the system level,
+        # it will also be disabled at the home-manager level
+        default = config.system.desktop-environment.enable;
       };
   };
 
