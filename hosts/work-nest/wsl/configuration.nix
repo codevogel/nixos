b2c64fb4ed12ae4d6dce6bf469d78005c11400fe
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-05 16:15:40 +0100

    update config for wsl

--- hosts/work-nest/wsl/configuration.nix
+++ hosts/work-nest/wsl/configuration.nix
@@ -1,7 +1,14 @@
-{ ... }:
+{ inputs, lib, ... }:
 
 {
   imports = [
     ../../common.nix
+    inputs.nixos-wsl.nixosModules.default
   ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  host-options.desktop-environment.enable = false;
+
+  wsl.enable = true;
+  wsl.defaultUser = "codevogel";
 }
