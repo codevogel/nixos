95ba90adc0a68d102fcf8ee569ee91a6fb47a0ca
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-06 17:07:10 +0100

    fix zsh

--- modules/common/home-manager/dev/shell/zsh.nix
+++ modules/common/home-manager/dev/shell/zsh.nix
@@ -7,8 +7,15 @@
 
 {
   config = lib.mkIf osConfig.host-options.home-manager.dev.shell.zsh.enable {
-    home.packages = with pkgs; [
-      zsh
-    ];
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      history.size = 10000;
+      oh-my-zsh = {
+        enable = true;
+      };
+    };
   };
 }
