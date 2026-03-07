76278b551938dfa418ea3fa590caf031317dd061
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-07 12:37:02 +0100

    add input imports in walker and gazelle

--- modules/hm-modules/apps/gazelle.nix
+++ modules/hm-modules/apps/gazelle.nix
@@ -7,6 +7,10 @@
 }:
 
 {
+  imports = [
+    inputs.gazelle.homeModules.gazelle
+  ];
+
   config = lib.mkIf osConfig.my.features.apps.gazelle.enable {
     home.packages = [
       inputs.gazelle.packages.${pkgs.stdenv.hostPlatform.system}.default
--- modules/hm-modules/system/hyprland/walker.nix
+++ modules/hm-modules/system/hyprland/walker.nix
@@ -1,10 +1,15 @@
 {
   lib,
   osConfig,
+  inputs,
   ...
 }:
 
 {
+  imports = [
+    inputs.walker.homeManagerModules.default
+  ];
+
   config = lib.mkIf osConfig.my.features.system.hyprland.walker.enable {
     programs.walker.enable = true;
   };
