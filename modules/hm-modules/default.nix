a1e31135284419580428ee48f0028c3288c028ee
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-07 11:57:50 +0100

    mv module files

--- modules/hm-modules/default.nix
+++ modules/hm-modules/default.nix
@@ -2,18 +2,18 @@
 
 {
   imports = [
-    ./pls/pls.nix
-    ./tmux/tmux.nix
-    ./waybar/waybar.nix
-    ./firefox.nix
-    ./gazelle.nix
-    ./git.nix
-    ./hyprland.nix
-    ./kitty.nix
-    ./mako.nix
-    ./oh-my-posh.nix
-    ./walker.nix
-    ./yazi.nix
-    ./zsh.nix
+    ./apps/dev/pls/pls.nix
+    ./apps/dev/tmux/tmux.nix
+    ./system/hyprland/waybar/waybar.nix
+    ./apps/browser/firefox.nix
+    ./apps/gazelle.nix
+    ./apps/dev/git.nix
+    ./system/hyprland/hyprland.nix
+    ./apps/dev/kitty.nix
+    ./system/hyprland/mako.nix
+    ./apps/dev/oh-my-posh.nix
+    ./system/hyprland/walker.nix
+    ./apps/file-explorer/yazi.nix
+    ./apps/dev/zsh.nix
   ];
 }
