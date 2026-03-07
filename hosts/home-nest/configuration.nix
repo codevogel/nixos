6a77bd4d67ea1f3949a5307fe969d90577baeb87
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-07 12:24:07 +0100

    rm old paths

--- hosts/home-nest/configuration.nix
+++ hosts/home-nest/configuration.nix
@@ -4,7 +4,6 @@
   imports = [
     ../common.nix
     ./hardware-configuration.nix
-    ../../modules/home-nest/default.nix
   ];
 
   my.features.apps.steam.enable = true;
--- hosts/work-nest/configuration.nix
+++ hosts/work-nest/configuration.nix
@@ -4,7 +4,6 @@
   imports = [
     ../common.nix
     ./hardware-configuration.nix
-    ../../modules/work-nest/default.nix
   ];
 
   my.features.system.hyprland.waybar.laptopModules.enable = true;
