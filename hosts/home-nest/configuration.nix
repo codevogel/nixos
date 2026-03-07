9a03a89109c467a57e260a470258ab2897d4956e
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-07 12:58:30 +0100

    add host-specific config

--- hosts/home-nest/configuration.nix
+++ hosts/home-nest/configuration.nix
@@ -7,4 +7,39 @@
   ];
 
   my.features.apps.steam.enable = true;
+  my.features.system.nvidia.enable = true;
+
+  hardware.nvidia.open = true;
+
+  networking = {
+    hostName = "home-nest";
+    networkmanager = {
+      # Enabled by my.features.system.networking.enable, but we need to set the profiles here.
+      ensureProfiles.profiles = {
+        wired-enp34s0 = {
+          connection = {
+            id = "Wired connection 1";
+            type = "802-3-ethernet";
+            interface-name = "enp34s0";
+            autoconnect = true;
+          };
+
+          ethernet = {
+            auto-negotiate = true;
+            speed = 1000;
+            duplex = "full";
+          };
+
+          ipv4 = {
+            method = "auto";
+          };
+
+          ipv6 = {
+            method = "auto";
+          };
+        };
+      };
+    };
+  };
+
 }
--- hosts/work-nest/configuration.nix
+++ hosts/work-nest/configuration.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ lib, config, ... }:
 
 {
   imports = [
@@ -7,4 +7,63 @@
   ];
 
   my.features.system.hyprland.waybar.laptopModules.enable = true;
+
+  stylix = {
+    fonts = {
+      sizes.applications = lib.mkForce 12;
+    };
+  };
+
+  networking = {
+    hostName = lib.mkForce "work-nest";
+    networkmanager = {
+      # Enabled by my.features.system.networking.enable, but we need to set the profiles here.
+      ensureProfiles = {
+        environmentFiles = [ config.sops.templates.eduroam-env.path ];
+        profiles = {
+          eduroam = {
+            connection = {
+              id = "eduroam";
+              type = "wifi";
+              interface-name = "wlp0s20f3";
+            };
+            wifi = {
+              mode = "infrastructure";
+              ssid = "eduroam";
+            };
+            wifi-security = {
+              key-mgmt = "wpa-eap";
+            };
+            "802-1x" = {
+              eap = "tls";
+              identity = "$EDUROAM_IDENTITY";
+              client-cert = "/etc/ssl/certs/eduroam/cert.pem";
+              private-key = "/etc/ssl/certs/eduroam/private.key";
+              private-key-password = "$EDUROAM_CERT_PASSWORD";
+              ca-cert = "/etc/ssl/certs/ca-bundle.crt";
+            };
+            ipv4 = {
+              method = "auto";
+            };
+            ipv6 = {
+              method = "auto";
+            };
+          };
+        };
+
+      };
+
+    };
+  };
+
+  sops = {
+    secrets = {
+      eduroam-identity = { };
+      eduroam-cert-password = { };
+    };
+    templates.eduroam-env.content = ''
+      EDUROAM_IDENTITY=${config.sops.placeholder.eduroam-identity}
+      EDUROAM_CERT_PASSWORD=${config.sops.placeholder.eduroam-cert-password}
+    '';
+  };
 }
