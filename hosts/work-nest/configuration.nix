1bad20035299013f9f28ff8d3a882ef611b943dc
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-09 08:48:28 +0100

    add hyprland monitor settings for work-nest

--- hosts/work-nest/configuration.nix
+++ hosts/work-nest/configuration.nix
@@ -14,6 +14,20 @@
     };
   };
 
+  home-manager.users."codevogel" = {
+    wayland.windowManager.hyprland = {
+      settings = {
+        monitor = [
+          "eDP-1, 1920x1200, 0x0, 1"
+          "DP-2, 1920x1080, 1920x0, 1"
+        ];
+        input = {
+          kb_options = "ctrl:nocaps";
+        };
+      };
+    };
+  };
+
   networking = {
     hostName = lib.mkForce "work-nest";
     networkmanager = {
