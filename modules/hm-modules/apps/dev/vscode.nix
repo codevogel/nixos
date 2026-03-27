3c7ff27ef8d588b2f6969415c70f6fae8151a3bf
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-27 16:48:40 +0100

    define vscode extensions for vim / csharp

--- modules/hm-modules/apps/dev/vscode.nix
+++ modules/hm-modules/apps/dev/vscode.nix
@@ -1,6 +1,7 @@
 {
   lib,
   osConfig,
+  pkgs,
   ...
 }:
 
@@ -8,6 +9,28 @@
   config = lib.mkIf osConfig.my.features.apps.dev.vscode.enable {
     programs.vscode = {
       enable = true;
+
+      profiles.default.extensions =
+        with pkgs.vscode-extensions;
+        [
+          vscodevim.vim
+        ]
+        ++ lib.optionals osConfig.my.features.apps.dev.vscode.extensions.csharp.enable [
+          csharpier.csharpier-vscode
+          ms-dotnettools.csharp
+        ];
     };
+
+    programs.nix-ld.libraries =
+      lib.optionals osConfig.my.features.apps.dev.vscode.extensions.csharp.enable
+        (
+          with pkgs;
+          [
+            stdenv.cc.cc.lib
+            zlib
+            openssl
+            icu
+          ]
+        );
   };
 }
