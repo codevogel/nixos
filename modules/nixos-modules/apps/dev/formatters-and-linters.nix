dd93ccd04135eedb9dfecffe392e17e234a0dcac
Kamiel de Visser <kamieldevisser@gmail.com>
2026-03-27 17:20:29 +0100

    add csharpier

--- modules/nixos-modules/apps/dev/formatters-and-linters.nix
+++ modules/nixos-modules/apps/dev/formatters-and-linters.nix
@@ -7,10 +7,17 @@
 
 {
   config = lib.mkIf config.my.features.apps.dev.formatters.enable {
-    environment.systemPackages = with pkgs; [
-      prettier
-      gdtoolkit_4
-      markdownlint-cli2
-    ];
+    environment.systemPackages =
+      with pkgs;
+      [
+        prettier
+        markdownlint-cli2
+      ]
+      ++ lib.optionals config.my.features.apps.dev.godot.enable [
+        gdtoolkit_4
+      ]
+      ++ lib.optionals config.my.features.apps.dev.dotnet.enable [
+        csharpier
+      ];
   };
 }
