9b5af2ff438d62352da10635f2f6d85e521c39c9
Kamiel de Visser <kamieldevisser@gmail.com>
2026-04-06 11:53:09 +0200

    make vscode vscode-fhs

--- modules/hm-modules/apps/dev/vscode.nix
+++ modules/hm-modules/apps/dev/vscode.nix
@@ -9,42 +9,7 @@
   config = lib.mkIf osConfig.my.features.apps.dev.vscode.enable {
     programs.vscode = {
       enable = true;
-
-      profiles.default = {
-        enableUpdateCheck = false;
-        enableExtensionUpdateCheck = false;
-        extensions =
-          with pkgs.vscode-extensions;
-          [
-            vscodevim.vim
-          ]
-          ++ lib.optionals osConfig.my.features.apps.dev.vscode.extensions.csharp.enable [
-            csharpier.csharpier-vscode
-            ms-dotnettools.csharp
-            ms-dotnettools.csdevkit
-            ms-dotnettools.vscode-dotnet-runtime
-            ms-dotnettools.vscodeintellicode-csharp
-          ]
-          ++ lib.optionals osConfig.my.features.apps.dev.vscode.extensions.nix.enable [
-            bbenoist.nix
-          ]
-          ++ lib.optionals osConfig.my.features.apps.dev.vscode.extensions.copilot.enable [
-            github.copilot
-            github.copilot-chat
-          ]
-          ++ lib.optionals osConfig.my.features.apps.dev.vscode.extensions.godot.enable [
-            geequlim.godot-tools
-          ]
-          ++
-            lib.optionals
-              (
-                osConfig.my.features.apps.dev.vscode.extensions.csharp.enable
-                && osConfig.my.features.apps.dev.vscode.extensions.godot.enable
-              )
-              [
-                woberg.godot-dotnet-tools
-              ];
-      };
+      package = pkgs.vscode-fhs;
     };
   };
 }
