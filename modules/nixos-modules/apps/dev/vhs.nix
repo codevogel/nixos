5095ec5d42367b644c169e35be69d46269961e9f
Kamiel de Visser <kamieldevisser@gmail.com>
2026-06-19 12:37:57 +0200

    pin vhs

--- modules/nixos-modules/apps/dev/vhs.nix
+++ modules/nixos-modules/apps/dev/vhs.nix
@@ -5,9 +5,18 @@
   ...
 }:
 
+let
+  pinnedPkgs =
+    import (fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/dd77db9265bc70cc04db61ee7f5be82a8c856926.tar.gz";
+      sha256 = "sha256:17pqcr98y3m85bz19qqf1yf3pckd6w4zbm1k3z43511my49ihym9";
+    }) {
+      system = pkgs.system;
+    };
+in
 {
   config = lib.mkIf config.my.features.apps.dev.vhs.enable {
-    environment.systemPackages = with pkgs; [
+    environment.systemPackages = with pinnedPkgs; [
       vhs
     ];
   };
